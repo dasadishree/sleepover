26c671abf8645656df5c78bffd69bb48ca332b8e
dasadishree <dasadishree@gmail.com>
2026-04-24 20:24:14 -0500

    firebase

---
+++ sleepover/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqJDtckPA1kZcVBRrhwmnB9W-XqJDQUsw",
+  authDomain: "sleepover-7a6cf.firebaseapp.com",
+  databaseURL: "https://sleepover-7a6cf-default-rtdb.firebaseio.com",
+  projectId: "sleepover-7a6cf",
+  storageBucket: "sleepover-7a6cf.firebasestorage.app",
+  messagingSenderId: "943335330317",
+  appId: "1:943335330317:web:20a1ae7eb8f71172d9fb44",
+  measurementId: "G-52J5XY5KGF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
