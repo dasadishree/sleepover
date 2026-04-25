396cd44cbb0de8095b9c1238d5953f088f56dff4
Jane <yevheniiasimaka@gmail.com>
2026-04-24 21:08:59 -0500

    change the location  of content from APp.jsx to Actor

--- sleepover/src/App.jsx
+++ sleepover/src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Actorpg from "./components/actorpg";
 
 function App() {
 	return (
-		<div className="w-[95vw] h-screen py-[2vh] mx-auto">
-			<img
-				src="/imgs/paper.jpg"
-				className="fixed top-0 left-0 w-full h-screen object-cover z-0"
-				alt=""
-			/>
-			<div className="z-10 relative">
-				<h1 className="text-[10vh] vintageone tracking-[-0.15vw] text-black">
-					I have a dream...
-				</h1>
-			</div>
-		</div>
+		<>
+			<Actorpg />
+		</>
 	);
 }
 
--- sleepover/src/components/Actorpg.jsx
+++ sleepover/src/components/Actorpg.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Actorpg = () => {
-	return <div></div>;
+	return (
+		<div className="w-[95vw] h-screen py-[2vh] mx-auto">
+			<img
+				src="/imgs/paper.jpg"
+				className="fixed top-0 left-0 w-full h-screen object-cover z-0"
+				alt=""
+			/>
+			<div className="z-10 relative">
+				<h1 className="text-[10vh] vintageone tracking-[-0.15vw] text-black">
+					I have a dream...
+				</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Actorpg;
