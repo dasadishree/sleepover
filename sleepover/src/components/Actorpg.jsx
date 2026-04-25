28c45113d0a8b4e31be0602d291b7ef5480f0e59
Jane <yevheniiasimaka@gmail.com>
2026-04-24 23:25:31 -0500

    new Reem

--- sleepover/src/components/Actorpg.jsx
+++ sleepover/src/components/Actorpg.jsx
@@ -8,7 +8,7 @@ const Actorpg = () => {
 				className="fixed top-0 left-0 w-full h-screen object-cover z-0"
 				alt=""
 			/>
-			<div className="z-10 relative">
+			<div className="z-10 relative h-screen">
 				<div className="flex items-center justify-between w-full">
 					<h1 className="text-[10vh] vintageone tracking-[-0.15vw] text-black">
 						d<i>REEM</i>oji...
@@ -17,16 +17,20 @@ const Actorpg = () => {
 						(Actor)
 					</h1>
 				</div>
-				<div className="flex items-center justify-center w-fit absolute top-[0vh] -translate-y-[10vh] right-[2vw] gap-[0vw]">
+				<div className="flex flex-col items-center justify-center w-fit absolute bottom-[0vh] bg-blue-500/0 right-[-2.5vw] gap-[0vw] h-fit">
 					<img
 						src="/emojis/yay.gif"
-						className="top-[20vh] left-[6.5vw] absolute w-[6vw]"
+						className="top-[25vh] left-[6.5vw] absolute w-[6vw]"
 						alt="emoji"
 					/>
-					<img src="/imgs/dream.png" className="w-[20vw]" alt="" />
 					<img
-						src="/imgs/reem.png"
-						className="mt-[45vh] w-[12vw] border-3 border-dotted rounded-[1vh]"
+						src="/imgs/dream.png"
+						className="w-[20vw] mr-auto translate-y-[18vh]"
+						alt=""
+					/>
+					<img
+						src="/imgs/reem-new.png"
+						className="w-[25vw] ml-[13vw] rounded-[1vh] -scale-x-100"
 						alt=""
 					/>
 				</div>
