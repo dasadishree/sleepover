5bcd5991250e1e03e2107a15cff09a810cf37f43
Jane <yevheniiasimaka@gmail.com>
2026-04-25 09:24:08 -0500

    added guesser route

--- sleepover/src/App.jsx
+++ sleepover/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Actorpg from "./components/actorpg";
 import Start from "./components/Start";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Guesserpg from "./components/Guesserpg";
 
 function App() {
 	return (
 		<BrowserRouter>
-		  <Routes>
-        <Route path="/" element={<Start/>}/>
-        <Route path="/actor" element={<Actorpg/>}/>
-      </Routes>
+			<Routes>
+				<Route path="/" element={<Start />} />
+				<Route path="/actor" element={<Actorpg />} />
+				<Route path="/guesser" element={<Guesserpg />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
